f78657a91d67ebb7a4293cd0e993657f243cadcc
AjayaKumarM <160041448+AjayaKumarM@users.noreply.github.com>
2024-04-11 20:57:25 +0530

    Update SwapTwoNumbers2.java

--- Java/Swap numbers/src/SwapTwoNumbers2.java
+++ Java/Swap numbers/src/SwapTwoNumbers2.java
@@ -7,9 +7,9 @@ public class SwapTwoNumbers2 {
 
         System.out.println("Before swapping: a = "+a+ ", b = "+b);
 
-        a = a+b;
-        b = a-b;
-        a = a-b;
+        a = a+b;  // a = 10+20 = 30;
+        b = a-b;  // b = 30-20 = 10;
+        a = a-b;  // a = 30-10 = 20;
 
         System.out.println("After swapping: a = "+a+ ", b = "+b);
 
